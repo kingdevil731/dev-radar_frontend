bb69c4058954978f67fe5b93743d846788f3de0a
ruben forner <9657174+kingdevil731@users.noreply.github.com>
2021-03-26 10:38:02 +0200

    api fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 const api = axios.create({
-  baseURL: "https://dev-radar-backend.herokuapp.com",
+  baseURL: "https://devradar0.herokuapp.com",
 });
 
 export default api;
